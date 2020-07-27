3dbc6fd752ab81dc8f972b12ed31075435e277aa
zhujs <984370133@qq.com>
2020-07-27 10:59:18 +0800

    add the first version

--- src/main/java/com/simpledev/App.java
+++ src/main/java/com/simpledev/App.java
@@ -3,5 +3,6 @@ package com.simpledev;
 public class App {
     public static void main(String ... args) {
         System.out.println("----start app ... ");
+        System.out.println("----app close ...");
     }
 }
