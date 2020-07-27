83fe60caa2a521f8b0e625ef4442a76e0b45be65
zhujs <984370133@qq.com>
2020-07-27 11:01:38 +0800

    add spring

--- src/main/java/com/simpledev/App.java
+++ src/main/java/com/simpledev/App.java
@@ -1,7 +1,11 @@
 package com.simpledev;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String ... args) {
         System.out.println("----start app ... ");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     }
 }
