c01b899fde9abedff952d9ff6828b84ccce8033b
zhujs <984370133@qq.com>
2020-07-29 20:36:57 +0800

    fix

--- src/main/java/com/simpledev/App.java
+++ src/main/java/com/simpledev/App.java
@@ -3,7 +3,7 @@ package com.simpledev;
 import com.simpledev.base.event.EventBus;
 import com.simpledev.base.scheduler.SimpleScheduler;
 import com.simpledev.base.scheduler.TimerEvent;
-import com.simpledev.dev.test.event.PlayerEvent1;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.util.concurrent.TimeUnit;
@@ -13,8 +13,6 @@ public class App {
     public void start() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         EventBus.init(context);
-        EventBus.post(new PlayerEvent1());
-        EventBus.post(new PlayerEvent1());
 
         SimpleScheduler.addTimerEvent(new TimerEvent(() -> {
             System.out.println("----5秒后执行");
