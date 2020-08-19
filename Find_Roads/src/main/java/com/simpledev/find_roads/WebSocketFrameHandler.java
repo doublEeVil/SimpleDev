d24e78b90092d1a782624c354082348c95f40951
sahngguyi <984370133@qq.com>
2020-08-20 00:15:59 +0800

    js

--- Find_Roads/src/main/java/com/simpledev/find_roads/WebSocketFrameHandler.java
+++ Find_Roads/src/main/java/com/simpledev/find_roads/WebSocketFrameHandler.java
@@ -1,10 +1,13 @@
 package com.simpledev.find_roads;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
+import java.nio.ByteBuffer;
 import java.util.Locale;
 
 public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
@@ -12,13 +15,18 @@ public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocket
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
         // ping and pong frames already handled
 
+        // System.out.println("---rcv: " + frame.getClass().getName());
         if (frame instanceof TextWebSocketFrame) {
             // Send the uppercase string back.
             String request = ((TextWebSocketFrame) frame).text();
             ctx.channel().writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
-        } else {
-            String message = "unsupported frame type: " + frame.getClass().getName();
-            throw new UnsupportedOperationException(message);
+        } else if (frame instanceof BinaryWebSocketFrame) {
+            BinaryWebSocketFrame webSocketFrame = (BinaryWebSocketFrame)frame;
+            ByteBuf buf =webSocketFrame.content();
+            buf.markReaderIndex();
+            buf.readInt();
+            buf.readInt();
+            System.out.println("x: " + buf.readInt() + " y:" + buf.readInt());
         }
     }
 }
