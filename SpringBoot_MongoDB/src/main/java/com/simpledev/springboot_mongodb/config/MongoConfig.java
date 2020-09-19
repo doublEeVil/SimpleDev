d51dd9dedaea70871ba996491e3d9d4f5c831aa4
zhujs <984370133@qq.com>
2020-09-19 12:20:52 +0800

    改用需认证的mongo

--- SpringBoot_MongoDB/src/main/java/com/simpledev/springboot_mongodb/config/MongoConfig.java
+++ SpringBoot_MongoDB/src/main/java/com/simpledev/springboot_mongodb/config/MongoConfig.java
@@ -5,6 +5,7 @@ import com.simpledev.springboot_mongodb.converter.CommObjAWriteConverter;
 import com.simpledev.springboot_mongodb.converter.CommObjBReadConverter;
 import com.simpledev.springboot_mongodb.converter.CommObjBWriteConverter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -16,19 +17,7 @@ import java.util.List;
 
 @Configuration
 @EnableAutoConfiguration
-public class MongoConfig extends AbstractMongoClientConfiguration
-{
-    @Override
-    protected String getDatabaseName() {
-        return "db_1";
-    }
-
-    @Override
-    protected void configureConverters(MongoCustomConversions.MongoConverterConfigurationAdapter adapter) {
-        adapter.registerConverter(new CommObjAReadConverter());
-        adapter.registerConverter(new CommObjAWriteConverter());
-        super.configureConverters(adapter);
-    }
+public class MongoConfig {
 
     @Bean
     public MongoCustomConversions customConversions() {
--- SpringBoot_MongoDB/src/main/java/com/simpledev/springboot_mongodb/controller/HelloController.java
+++ SpringBoot_MongoDB/src/main/java/com/simpledev/springboot_mongodb/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.simpledev.springboot_mongodb.controller;
 
 import com.simpledev.springboot_mongodb.dao.LimitUserDao;
+import com.simpledev.springboot_mongodb.dao.UserDao;
 import com.simpledev.springboot_mongodb.entity.LimitUser;
+import com.simpledev.springboot_mongodb.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,14 @@ import javax.annotation.Resource;
 public class HelloController {
 
     @Autowired
-    private LimitUserDao dao;
+    private LimitUserDao limitUserDao;
+    @Autowired
+    private UserDao userDao;
 
-    @GetMapping("/")
+    @GetMapping("/add")
     public LimitUser q() {
-        return dao.find(124);
+        userDao.save(new User());
+        //limitUserDao.save(new LimitUser());
+        return null;
     }
 }
