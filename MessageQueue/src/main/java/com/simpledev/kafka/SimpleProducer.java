3a46c5faeeefa7acb1a2843e2abdb7bd3c6706c5
sahngguyi <984370133@qq.com>
2020-11-11 16:54:17 +0800

    增加一个kafka的例子

---
+++ MessageQueue/src/main/java/com/simpledev/kafka/SimpleProducer.java
@@ -0,0 +1,48 @@
+package com.simpledev.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+
+    public static void main(String ... args) {
+        Properties props = new Properties();
+        //Assign localhost id
+        props.put("bootstrap.servers", "localhost:9092");
+
+        //Set acknowledgements for producer requests.
+        props.put("acks", "all");
+
+        //If the request fails, the producer can automatically retry,
+        props.put("retries", 0);
+
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
+
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        String topicName = "my_test";
+        int i = 10;
+        while (i-- > 0) {
+            producer.send(new ProducerRecord<String, String>(topicName,
+                    String.valueOf(i),
+                    String.valueOf(i)));
+            System.out.printf("---send msg:%s\n", i);
+        }
+        producer.close();
+    }
+}
