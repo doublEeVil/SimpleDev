f795ac288fd68f745aab0c7e6cbe283ef49ada60
jinshan <984370133@qq.com>
2021-05-05 10:18:06 +0800

    增加bill

---
+++ Bill/src/main/resources/test_2021-05-05.sql
@@ -0,0 +1,49 @@
+# ************************************************************
+# Sequel Ace SQL dump
+# Version 2121
+#
+# https://sequel-ace.com/
+# https://github.com/Sequel-Ace/Sequel-Ace
+#
+# Host: 127.0.0.1 (MySQL 8.0.22)
+# Database: test
+# Generation Time: 2021-05-05 02:16:00 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table bill
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bill`;
+
+CREATE TABLE `bill` (
+  `id` int NOT NULL,
+  `amt` double DEFAULT NULL,
+  `create_at` datetime(6) DEFAULT NULL,
+  `date_time` datetime(6) DEFAULT NULL,
+  `rcv_type` varchar(255) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `update_at` datetime(6) DEFAULT NULL,
+  `use_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
